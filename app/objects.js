094df0114ad652e344ef8d8c82272d7fa832d377
Max <max_95_26@hotmail.com>
2020-12-19 18:50:50 -0500

    Objects ok

--- app/objects.js
+++ app/objects.js
@@ -1,15 +1,11 @@
-exports = typeof window === 'undefined' ? global : window;
+exports = typeof window === "undefined" ? global : window;
 
 exports.objectsAnswers = {
-  alterContext: function(fn, obj) {
-
-  },
-
-  alterObjects: function(constructor, greeting) {
-
-  },
-
-  iterate: function(obj) {
-
-  }
+  alterContext: (fn, obj) => fn.call(obj),
+  alterObjects: (constructor, greeting) =>
+    (constructor.prototype.greeting = greeting),
+  iterate: (obj) =>
+    Object.keys(obj).map((value) => {
+      return value + ": " + obj[value];
+    }),
 };
