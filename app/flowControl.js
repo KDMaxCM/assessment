ff0fd7b3518288d0643bf6b89fffc2d89bab760c
Max <max_95_26@hotmail.com>
2020-12-19 18:55:39 -0500

    Flow Control ok

--- app/flowControl.js
+++ app/flowControl.js
@@ -1,7 +1,7 @@
-exports = typeof window === 'undefined' ? global : window;
+exports = typeof window === "undefined" ? global : window;
 
 exports.flowControlAnswers = {
-  fizzBuzz: function(num) {
+  fizzBuzz: (num) => {
     // write a function that receives a number as its argument;
     // if the number is divisible by 3, the function should return 'fizz';
     // if the number is divisible by 5, the function should return 'buzz';
@@ -11,5 +11,14 @@ exports.flowControlAnswers = {
     // otherwise the function should return the number, or false if no number
     // was provided or the value provided is not a number
 
-  }
+    return num % 3 === 0
+      ? num % 5 === 0
+        ? "fizzbuzz"
+        : "fizz"
+      : num % 5 === 0
+      ? "buzz"
+      : isNaN(num)
+      ? false
+      : num;
+  },
 };
