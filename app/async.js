4756a82a7da18ab611075d23fa046c396fb6ab70
Max <max_95_26@hotmail.com>
2020-12-19 16:22:13 -0500

    Async ok

--- app/async.js
+++ app/async.js
@@ -1,11 +1,20 @@
-exports = typeof window === 'undefined' ? global : window;
+exports = typeof window === "undefined" ? global : window;
 
 exports.asyncAnswers = {
-  async: function(value) {
-
+  async: (value) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(function () {
+        resolve(value);
+      }, 100);
+    });
   },
 
-  manipulateRemoteData: function(url) {
-
-  }
+  manipulateRemoteData: (url) => {
+    const deferred = $.Deferred();
+    $.ajax(url).then(({ people }) => {
+      const peopleSort = people.map((person) => person.name).sort();
+      deferred.resolve(peopleSort);
+    });
+    return deferred.promise();
+  },
 };
