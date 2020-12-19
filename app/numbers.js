81792aa1a4bdf2032e78a2f02772cca17df9d103
Max <max_95_26@hotmail.com>
2020-12-19 18:47:09 -0500

    Numbers 3/4

--- app/numbers.js
+++ app/numbers.js
@@ -1,19 +1,24 @@
-exports = typeof window === 'undefined' ? global : window;
+exports = typeof window === "undefined" ? global : window;
 
 exports.numbersAnswers = {
-  valueAtBit: function(num, bit) {
+  valueAtBit: function (num, bit) {},
 
+  base10: (str) => parseInt(str, 2),
+
+  convertToBinary: (num) => {
+    const binary = Number(num).toString(2);
+    if (binary.length === 8) return binary;
+    let pendingValues = 8 - binary.length;
+    let pending = "";
+    for (let index = 0; index < pendingValues; index++) {
+      pending += "0";
+    }
+    return pending + binary;
   },
 
-  base10: function(str) {
+  multiply: (a, b) => {
+    const numbersOfDecimal = parseInt(String(b).split(".")[1].length);
 
+    return parseFloat((a * b).toFixed(numbersOfDecimal));
   },
-
-  convertToBinary: function(num) {
-
-  },
-
-  multiply: function(a, b) {
-
-  }
 };
