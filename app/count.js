445f904834becfdd315d1cf9735b219497038827
Max <max_95_26@hotmail.com>
2020-12-19 18:59:17 -0500

    Count ok

--- app/count.js
+++ app/count.js
@@ -1,5 +1,17 @@
 exports = typeof window === "undefined" ? global : window;
 
 exports.countAnswers = {
-  count: function (start, end) {},
+  count: (start, end) => {
+    let _timeOut = null;
+
+    const executeTimeOut = () => {
+      console.log(start++);
+      if (start <= end) _timeOut = setTimeout(executeTimeOut, 100);
+    };
+    executeTimeOut();
+
+    return {
+      cancel: () => clearTimeout(_timeOut),
+    };
+  },
 };
